89014e0b3ec9f608b2ec6559926874c037059833
Missael Alejandro Bernabe Moran <127263606+MissaelBM@users.noreply.github.com>
2024-10-15 15:23:50 +0000

    Version final

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express=require("express")
-const app = express()
-app.get("/",(req, res)=>{
-  res.send("Hola Mundo")
-})
+// server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-app.listen(3000, (req,res)=>{
-    console.log("Corre en el puerto 3000")
-})
+// Crea el servidor HTTP
+const server = http.createServer((req, res) => {
+  // Lee el archivo HTML y envíalo como respuesta
+  fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error: No se pudo cargar el archivo');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+// El servidor escucha en el puerto 3000
+server.listen(3000, () => {
+  console.log('Servidor ejecutándose en http://localhost:3000');
+});
